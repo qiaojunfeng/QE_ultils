7454e4ebf8c3f4831ebc58d3af404075b1070c57
qiaojunfeng <qiaojunfeng@outlook.com>
2017-01-13 12:07:17 +0800

    add makefile, including automatic tests

---
+++ makefile
@@ -0,0 +1,18 @@
+F90=gfortran
+
+default: constants kinds
+	$(F90) -o pwuc pwuc.f90
+	./pwuc ry2ev 1
+	./pwuc ev2ry 1
+	./pwuc au2ev 1
+	./pwuc ev2au 1
+	./pwuc b2a 1
+	./pwuc a2b 1
+	./pwuc ls
+	./pwuc
+
+constants: kinds
+	$(F90) -c constants.f90
+
+kinds:
+	$(F90) -c kind.f90
--- pwuc.f90
+++ pwuc.f90
@@ -1,103 +1,56 @@
 ! list all available constants and derived values
 
 PROGRAM pwuc
-  USE kinds, ONLY : DP
-  USE constants
+    USE kinds, ONLY : DP
+    USE constants
 
-  implicit none
-  integer :: narg, stat
-  character(len=256) :: argkind, argval
-  real (DP) :: inval, outval
+    implicit none
+    integer :: narg, stat
+    character(len=256) :: argkind, argval
+    real (DP) :: inval, outval
 
-  call get_command(status=stat)
-  if (stat /= 0) then
-    call print_help()
-    stop
-  end if
+    call get_command(status=stat)
+    if (stat /= 0) then
+        call print_help()
+        stop
+    end if
 
-  narg = command_argument_count()
-  if (narg /= 2) then
-    call print_help()
-    stop
-  end if
+    narg = command_argument_count()
+    if (narg == 1) then
+        call get_command_argument(1, argkind)
+        if (argkind == 'ls') then
+            call ls_const()
+            stop
+        else
+            call print_help()
+            stop
+        end if
+    elseif (narg /= 2) then
+        call print_help()
+        stop
+    end if
 
-  call get_command_argument(1, argkind)
-  call get_command_argument(2, argval)
-  read(argval, *) inval
-  if (argkind == 'ry2ev') then
-    outval = inval * RYTOEV
-  elseif (argkind == 'ev2ry') then
-    outval = inval / RYTOEV
-  elseif (argkind == 'au2ev') then
-    outval = inval * AUTOEV
-  elseif (argkind == 'ev2au') then
-    outval = inval / AUTOEV
-  elseif (argkind == 'b2a') then
-    outval = inval * BOHR_RADIUS_ANGS
-  elseif (argkind == 'a2b') then
-    outval = inval / BOHR_RADIUS_ANGS
-  else
-    call print_help()
-    stop
-  end if
+    call get_command_argument(1, argkind)
+    call get_command_argument(2, argval)
+    read(argval, *) inval
+    if (argkind == 'ry2ev') then
+        outval = inval * RYTOEV
+    elseif (argkind == 'ev2ry') then
+        outval = inval / RYTOEV
+    elseif (argkind == 'au2ev') then
+        outval = inval * AUTOEV
+    elseif (argkind == 'ev2au') then
+        outval = inval / AUTOEV
+    elseif (argkind == 'b2a') then
+        outval = inval * BOHR_RADIUS_ANGS
+    elseif (argkind == 'a2b') then
+        outval = inval / BOHR_RADIUS_ANGS
+    else
+        call print_help()
+        stop
+    end if
 
-  write(*,'(G24.17)') outval
-
-! WRITE (*,'(A18,G24.17)') 'pi:',pi
-! WRITE (*,'(A18,G24.17)') 'tpi:',tpi
-! WRITE (*,'(A18,G24.17)') 'fpi:',fpi
-! WRITE (*,'(A18,G24.17)') 'sqrtpi:',sqrtpi
-! WRITE (*,'(A18,G24.17)') 'sqrtpm1:',sqrtpm1
-! WRITE (*,'(A18,G24.17)') 'sqrt2:',sqrt2
-! WRITE (*,'(A18,G24.17)') 'H_PLANCK_SI:',H_PLANCK_SI
-! WRITE (*,'(A18,G24.17)') 'K_BOLTZMANN_SI:',K_BOLTZMANN_SI
-! WRITE (*,'(A18,G24.17)') 'ELECTRON_SI:',ELECTRON_SI
-! WRITE (*,'(A18,G24.17)') 'ELECTRONVOLT_SI:',ELECTRONVOLT_SI
-! WRITE (*,'(A18,G24.17)') 'ELECTRONMASS_SI:',ELECTRONMASS_SI
-! WRITE (*,'(A18,G24.17)') 'HARTREE_SI:',HARTREE_SI
-! WRITE (*,'(A18,G24.17)') 'RYDBERG_SI:',RYDBERG_SI
-! WRITE (*,'(A18,G24.17)') 'BOHR_RADIUS_SI:',BOHR_RADIUS_SI
-! WRITE (*,'(A18,G24.17)') 'AMU_SI:',AMU_SI
-! WRITE (*,'(A18,G24.17)') 'C_SI:',C_SI
-! WRITE (*,'(A18,G24.17)') 'MUNOUGHT_SI:',MUNOUGHT_SI
-! WRITE (*,'(A18,G24.17)') 'EPSNOUGHT_SI:',EPSNOUGHT_SI
-! WRITE (*,'(A18,G24.17)') 'K_BOLTZMANN_AU:',K_BOLTZMANN_AU
-! WRITE (*,'(A18,G24.17)') 'K_BOLTZMANN_RY:',K_BOLTZMANN_RY
-! WRITE (*,'(A18,G24.17)') 'AUTOEV:',AUTOEV
-! WRITE (*,'(A18,G24.17)') 'RYTOEV:',RYTOEV
-! WRITE (*,'(A18,G24.17)') 'AMU_AU:',AMU_AU
-! WRITE (*,'(A18,G24.17)') 'AMU_RY:',AMU_RY
-! WRITE (*,'(A18,G24.17)') 'AU_SEC:',AU_SEC
-! WRITE (*,'(A18,G24.17)') 'AU_PS:',AU_PS
-! WRITE (*,'(A18,G24.17)') 'AU_GPA:',AU_GPA
-! WRITE (*,'(A18,G24.17)') 'RY_KBAR:',RY_KBAR
-! WRITE (*,'(A18,G24.17)') 'DEBYE_SI:',DEBYE_SI
-! WRITE (*,'(A18,G24.17)') 'AU_DEBYE:',AU_DEBYE
-! WRITE (*,'(A18,G24.17)') 'eV_to_kelvin:',eV_to_kelvin
-! WRITE (*,'(A18,G24.17)') 'ry_to_kelvin:',ry_to_kelvin
-! WRITE (*,'(A18,G24.17)') 'EVTONM:',EVTONM
-! WRITE (*,'(A18,G24.17)') 'RYTONM:',RYTONM
-! WRITE (*,'(A18,G24.17)') 'C_AU:',C_AU
-! WRITE (*,'(A18,G24.17)') 'eps4:',eps4
-! WRITE (*,'(A18,G24.17)') 'eps6:',eps6
-! WRITE (*,'(A18,G24.17)') 'eps8:',eps8
-! WRITE (*,'(A18,G24.17)') 'eps12:',eps12
-! WRITE (*,'(A18,G24.17)') 'eps14:',eps14
-! WRITE (*,'(A18,G24.17)') 'eps16:',eps16
-! WRITE (*,'(A18,G24.17)') 'eps24:',eps24
-! WRITE (*,'(A18,G24.17)') 'eps32:',eps32
-! WRITE (*,'(A18,G24.17)') 'gsmall:',gsmall
-! WRITE (*,'(A18,G24.17)') 'e2:',e2
-! WRITE (*,'(A18,G24.17)') 'degspin:',degspin
-! WRITE (*,'(A18,G24.17)') 'BOHR_RADIUS_CM:',BOHR_RADIUS_CM
-! WRITE (*,'(A18,G24.17)') 'BOHR_RADIUS_ANGS:',BOHR_RADIUS_ANGS
-! WRITE (*,'(A18,G24.17)') 'ANGSTROM_AU:',ANGSTROM_AU
-! WRITE (*,'(A18,G24.17)') 'DIP_DEBYE:',DIP_DEBYE
-! WRITE (*,'(A18,G24.17)') 'AU_TERAHERTZ:',AU_TERAHERTZ
-! WRITE (*,'(A18,G24.17)') 'AU_TO_OHMCMM1:',AU_TO_OHMCMM1
-! WRITE (*,'(A18,G24.17)') 'RY_TO_THZ:',RY_TO_THZ
-! WRITE (*,'(A18,G24.17)') 'RY_TO_GHZ:',RY_TO_GHZ
-! WRITE (*,'(A18,G24.17)') 'RY_TO_CMM1:',RY_TO_CMM1
+    write(*,'(G24.17)') outval
 
 END PROGRAM pwuc
 
@@ -113,10 +66,74 @@ subroutine print_help()
     &'    ev2ry: electron volt to Rydberg' // NEW_LINE('a') // &
     &'    au2ev: atomic unit to electron volt' // NEW_LINE('a') // &
     &'    ev2au: electron volt to atomic unit' // NEW_LINE('a') // &
-    &'    b2a  : bohr to angstrom' // NEW_LINE('a') //&
-    &'    a2b  : angstrom to bohr' // NEW_LINE('a')
+    &'    b2a  : bohr to angstrom' // NEW_LINE('a') // &
+    &'    a2b  : angstrom to bohr' // NEW_LINE('a') // &
+    &'    ls   : list all constant in QE'
 
     write(*, '(a)', ADVANCE='no') help_info
     write(*, '(a)') ''
 end subroutine print_help
 
+subroutine ls_const()
+! code generated by qe-6.0/Modules/constants.f90
+    USE kinds, ONLY : DP
+    USE constants
+    implicit none
+
+    write(*,'(A18,G24.17)') 'pi:',pi
+    write(*,'(A18,G24.17)') 'tpi:',tpi
+    write(*,'(A18,G24.17)') 'fpi:',fpi
+    write(*,'(A18,G24.17)') 'sqrtpi:',sqrtpi
+    write(*,'(A18,G24.17)') 'sqrtpm1:',sqrtpm1
+    write(*,'(A18,G24.17)') 'sqrt2:',sqrt2
+    write(*,'(A18,G24.17)') 'H_PLANCK_SI:',H_PLANCK_SI
+    write(*,'(A18,G24.17)') 'K_BOLTZMANN_SI:',K_BOLTZMANN_SI
+    write(*,'(A18,G24.17)') 'ELECTRON_SI:',ELECTRON_SI
+    write(*,'(A18,G24.17)') 'ELECTRONVOLT_SI:',ELECTRONVOLT_SI
+    write(*,'(A18,G24.17)') 'ELECTRONMASS_SI:',ELECTRONMASS_SI
+    write(*,'(A18,G24.17)') 'HARTREE_SI:',HARTREE_SI
+    write(*,'(A18,G24.17)') 'RYDBERG_SI:',RYDBERG_SI
+    write(*,'(A18,G24.17)') 'BOHR_RADIUS_SI:',BOHR_RADIUS_SI
+    write(*,'(A18,G24.17)') 'AMU_SI:',AMU_SI
+    write(*,'(A18,G24.17)') 'C_SI:',C_SI
+    write(*,'(A18,G24.17)') 'MUNOUGHT_SI:',MUNOUGHT_SI
+    write(*,'(A18,G24.17)') 'EPSNOUGHT_SI:',EPSNOUGHT_SI
+    write(*,'(A18,G24.17)') 'K_BOLTZMANN_AU:',K_BOLTZMANN_AU
+    write(*,'(A18,G24.17)') 'K_BOLTZMANN_RY:',K_BOLTZMANN_RY
+    write(*,'(A18,G24.17)') 'AUTOEV:',AUTOEV
+    write(*,'(A18,G24.17)') 'RYTOEV:',RYTOEV
+    write(*,'(A18,G24.17)') 'AMU_AU:',AMU_AU
+    write(*,'(A18,G24.17)') 'AMU_RY:',AMU_RY
+    write(*,'(A18,G24.17)') 'AU_SEC:',AU_SEC
+    write(*,'(A18,G24.17)') 'AU_PS:',AU_PS
+    write(*,'(A18,G24.17)') 'AU_GPA:',AU_GPA
+    write(*,'(A18,G24.17)') 'RY_KBAR:',RY_KBAR
+    write(*,'(A18,G24.17)') 'DEBYE_SI:',DEBYE_SI
+    write(*,'(A18,G24.17)') 'AU_DEBYE:',AU_DEBYE
+    write(*,'(A18,G24.17)') 'eV_to_kelvin:',eV_to_kelvin
+    write(*,'(A18,G24.17)') 'ry_to_kelvin:',ry_to_kelvin
+    write(*,'(A18,G24.17)') 'EVTONM:',EVTONM
+    write(*,'(A18,G24.17)') 'RYTONM:',RYTONM
+    write(*,'(A18,G24.17)') 'C_AU:',C_AU
+    write(*,'(A18,G24.17)') 'eps4:',eps4
+    write(*,'(A18,G24.17)') 'eps6:',eps6
+    write(*,'(A18,G24.17)') 'eps8:',eps8
+    write(*,'(A18,G24.17)') 'eps12:',eps12
+    write(*,'(A18,G24.17)') 'eps14:',eps14
+    write(*,'(A18,G24.17)') 'eps16:',eps16
+    write(*,'(A18,G24.17)') 'eps24:',eps24
+    write(*,'(A18,G24.17)') 'eps32:',eps32
+    write(*,'(A18,G24.17)') 'gsmall:',gsmall
+    write(*,'(A18,G24.17)') 'e2:',e2
+    write(*,'(A18,G24.17)') 'degspin:',degspin
+    write(*,'(A18,G24.17)') 'BOHR_RADIUS_CM:',BOHR_RADIUS_CM
+    write(*,'(A18,G24.17)') 'BOHR_RADIUS_ANGS:',BOHR_RADIUS_ANGS
+    write(*,'(A18,G24.17)') 'ANGSTROM_AU:',ANGSTROM_AU
+    write(*,'(A18,G24.17)') 'DIP_DEBYE:',DIP_DEBYE
+    write(*,'(A18,G24.17)') 'AU_TERAHERTZ:',AU_TERAHERTZ
+    write(*,'(A18,G24.17)') 'AU_TO_OHMCMM1:',AU_TO_OHMCMM1
+    write(*,'(A18,G24.17)') 'RY_TO_THZ:',RY_TO_THZ
+    write(*,'(A18,G24.17)') 'RY_TO_GHZ:',RY_TO_GHZ
+    write(*,'(A18,G24.17)') 'RY_TO_CMM1:',RY_TO_CMM1
+
+end subroutine ls_const
