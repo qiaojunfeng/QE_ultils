e08239d019d37ab362733359166920890eb9ee17
qiaojunfeng <qiaojunfeng@outlook.com>
2017-04-25 19:15:22 +0800

    remove all whitespace of grepfermi | pwuc output

--- mae/gen_proj_in.sh
+++ mae/gen_proj_in.sh
@@ -74,6 +74,8 @@ else
 fi
 FERMI=`grepfermi.sh | tail -2 | head -1`
 FERMIEV=`pwuc au2ev $FERMI`
+# remove all whitespace
+FERMIEV="$(echo -e $FERMIEV | tr -d '[:space:]' )"
 cd $PWDIR
 
 echo $FERMIEV
